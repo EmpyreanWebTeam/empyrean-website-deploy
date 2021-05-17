c2090560bb76d8115db4eb098df75779265258b0
Ryan Marshall <rmarsh000@gmail.com>
2021-05-17 13:56:51 -0700

    added basic react routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
+
+import Splash from './splash/Splash';
+import Index from './dashboard/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <ul>
+          <li>
+            <Link to="/">Splash</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Splash />
+          </Route>
+          <Route path="/dashboard">
+            <Index />
+          </Route>
+        </Switch>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/dashboard/Index.js
@@ -0,0 +1,10 @@
+
+function Index() {
+    return (
+      <div>
+        <h2>Index Page to Dashboard</h2>
+      </div>
+    );
+}
+
+export default Index;
