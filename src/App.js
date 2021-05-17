e183fd76048525393d5588301de78d71fc59d214
Ryan Marshall <rmarsh000@gmail.com>
2021-05-17 14:35:21 -0700

    added material UI to framework for styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,46 +7,47 @@ import {
 } from "react-router-dom";
 
 import './App.css';
-
+import DefaultLayout from './layouts/DefaultLayout';
 import Splash from './splash/Splash';
 import Index from './dashboard/Index';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-
-        <ul>
-          <li>
-            <Link to="/">Splash</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/">
-            <Splash />
-          </Route>
-          <Route path="/dashboard">
-            <Index />
-          </Route>
-        </Switch>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-      </div>
+      <DefaultLayout>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Splash</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <Splash />
+            </Route>
+            <Route path="/dashboard">
+              <Index />
+            </Route>
+          </Switch>
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header> */}
+        </div>
+      </DefaultLayout>
     </Router>
   );
 }
