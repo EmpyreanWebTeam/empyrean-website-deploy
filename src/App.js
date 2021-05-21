fcaf6f2e360710ccfd9fb7514ecad8b43386a5db
EmpyreanWebTeam <jon1969edwards@gmail.com>
2021-05-21 12:52:44 +0200

    Updates to yarn version 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { AnimatePresence } from "framer-motion";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,31 +15,42 @@ import Splash from './splash/Splash';
 import Dashboard from './dashboard/dashboard';
 
 function App() {
-  const imageDetails = {
-    width: 524,
-    height: 650,
-  };
-   
   return (
     <Router>
-      <Route
-        render={({ location }) => (
-          <AnimatePresence initial={false} exitBeforeEnter>
-            <Switch location={location} key={location.pathname}>
-              <Route
-                exact
-                path='/'
-                render={() => <Splash imageDetails={imageDetails} />}
-              />
-              <Route
-                exact
-                path='/dashboard/:id'
-                render={() => <Dashboard imageDetails={imageDetails} />}
-              />
-            </Switch>
-          </AnimatePresence>
-        )}
-      />
+      <DefaultLayout>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Splash</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <Splash />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+          {/* <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header> */}
+        </div>
+      </DefaultLayout>
     </Router>
   );
 }
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,79 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { motion, useViewportScroll, useTransform } from "framer-motion";
 
-//Components
-
-//Ease
-const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
-
-
-
-const Dashboard = ({ imageDetails }) => {
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.15]);
-
-  const [canScroll, setCanScroll] = useState(false);
-
-  useEffect(() => {
-    if (canScroll === false) {
-      document.querySelector("body").classList.add("no-scroll");
-    } else {
-      document.querySelector("body").classList.remove("no-scroll");
-    }
-  }, [canScroll]);
-
-  return (
-    <motion.div
-      onAnimationComplete={() => setCanScroll(true)}
-      className='single'
-      initial='initial'
-      animate='animate'
-      exit='exit'>
-      <div className='container fluid'>
-        <div className='row center top-row'>
-          <div className='top'>
-            
-          </div>
-        </div>
-        <div className='row bottom-row'>
-          <div className='bottom'>
-            <motion.div className='image-container-single'>
-              <motion.div
-                initial={{
-                  y: "-50%",
-                  width: imageDetails.width,
-                  height: imageDetails.height,
-                }}
-                animate={{
-                  y: -100,
-                  width: "100%",
-                  height: window.innerWidth > 1440 ? 800 : 350,
-                  transition: { delay: 0.2, ...transition },
-                }}
-                className='thumbnail-single'>
-                <motion.div
-                  className='frame-single'
-                  whileHover='hover'
-                  transition={transition}>
-                  <motion.img
-                    src={require("../images/swiperScreen.png")}
-                    alt='an image'
-                    style={{ scale: scale }}
-                    initial={{ scale: 1.0 }}
-                    animate={{
-                      transition: { delay: 0.2, ...transition },
-                      y: window.innerWidth > 1440 ? -1200 : -600,
-                    }}
-                  />
-                </motion.div>
-              </motion.div>
-            </motion.div>
-          </div>
-        </div>
+function Dashboard() {
+    return (
+      <div>
+        <h2>Index Page to Dashboard</h2>
       </div>
-      
-    </motion.div>
-  );
-};
+    );
+}
 
 export default Dashboard;
