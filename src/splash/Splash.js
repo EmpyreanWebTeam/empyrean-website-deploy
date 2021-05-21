5835ce9d8d8fb5791f0ca2a8e4084cc8f09983be
EmpyreanWebTeam <jon1969edwards@gmail.com>
2021-05-21 11:12:42 +0200

    Adds development branch, splash branch and swinging doors to splash branch

--- src/splash/Splash.js
+++ src/splash/Splash.js
@@ -1,8 +1,32 @@
+import React, { Component } from "react";
 
-export default function Home() {
+// Images
+import gLeft from '../images/ecGateLeft.png';
+import gRight from '../images/ecGateRight.png';
+import inside from '../images/swiperScreen.png';
+
+// Style
+import "../style/doors.css";
+ 
+class Splash extends Component {
+  render() {
     return (
-      <div>
-        <h2>Home</h2>
-      </div>
+      
+        <div className="wrapper">
+          <div className="gLeftandRight">
+              <div id="left-door" className="lDoor">
+                <img src={gLeft} className="gLeft" alt="leftDoor">
+                </img>
+              </div>
+                <img src={inside} alt="inside" className="inside"></img>
+              <div id="right-door" className="rDoor">
+                <img src={gRight} className="gRight" alt="rightDoor">
+                </img>
+              </div>
+          </div>
+        </div>
     );
+  }
 }
+ 
+export default Splash;
