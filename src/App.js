dd1f0b6acdfde6c3f205b56be54eadf98a37f812
EmpyreanWebTeam <jon1969edwards@gmail.com>
2021-05-21 11:59:56 +0200

    Adds code and 'framer-motion' module to the App.js to start the 'walk through door' effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-// import logo from './logo.svg';
+import React from "react";
+import { AnimatePresence } from "framer-motion";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,48 +7,40 @@ import {
   Link
 } from "react-router-dom";
 
+// Style
 import './App.css';
+
+// Pages
 import DefaultLayout from './layouts/DefaultLayout';
 import Splash from './splash/Splash';
-import Index from './dashboard/Index';
+import Dashboard from './dashboard/dashboard';
 
 function App() {
+  const imageDetails = {
+    width: 524,
+    height: 650,
+  };
+   
   return (
     <Router>
-      <DefaultLayout>
-        <div className="App">
-          <ul>
-            <li>
-              <Link to="/">Splash</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Splash />
-            </Route>
-            <Route path="/dashboard">
-              <Index />
-            </Route>
-          </Switch>
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header> */}
-        </div>
-      </DefaultLayout>
+      <Route
+        render={({ location }) => (
+          <AnimatePresence initial={false} exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route
+                exact
+                path='/'
+                render={() => <Splash imageDetails={imageDetails} />}
+              />
+              <Route
+                exact
+                path='/dashboard/:id'
+                render={() => <Dashboard imageDetails={imageDetails} />}
+              />
+            </Switch>
+          </AnimatePresence>
+        )}
+      />
     </Router>
   );
 }
