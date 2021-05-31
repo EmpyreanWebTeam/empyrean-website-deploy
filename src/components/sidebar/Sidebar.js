f31f8622a7bba979261f90521b624bb61a7a7856
jonedwards_dot_tech <jon@jonedwards.tech>
2021-05-31 12:45:03 +0000

    Adds 'home link' to dashboard page

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -20,58 +20,58 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
       <div className="sidebar__menu">
         <div className="sidebar__link active_menu_link">
           <i className="fa fa-home"></i>
-          <a href="../">Home</a>
+          <a href="../swiper/empyrean">Home</a>
         </div>
         <h2>MNG</h2>
         <div className="sidebar__link">
           <i className="fa fa-user-secret" aria-hidden="true"></i>
-          <a href="#">Admin Management</a>
+          <a href="../">Admin Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-building-o"></i>
-          <a href="#">Company Management</a>
+          <a href="../">Company Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-wrench"></i>
-          <a href="#">Employee Management</a>
+          <a href="../">Employee Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-archive"></i>
-          <a href="#">Warehouse</a>
+          <a href="../">Warehouse</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-handshake-o"></i>
-          <a href="#">Contracts</a>
+          <a href="../">Contracts</a>
         </div>
         <h2>LEAVE</h2>
         <div className="sidebar__link">
           <i className="fa fa-question"></i>
-          <a href="#">Requests</a>
+          <a href="../">Requests</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-sign-out"></i>
-          <a href="#">Leave Policy</a>
+          <a href="../">Leave Policy</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-calendar-check-o"></i>
-          <a href="#">Special Days</a>
+          <a href="../">Special Days</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-files-o"></i>
-          <a href="#">Apply for leave</a>
+          <a href="../">Apply for leave</a>
         </div>
         <h2>PAYROLL</h2>
         <div className="sidebar__link">
           <i className="fa fa-money"></i>
-          <a href="#">Payroll</a>
+          <a href="../">Payroll</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-briefcase"></i>
-          <a href="#">Paygrade</a>
+          <a href="../">Paygrade</a>
         </div>
         <div className="sidebar__logout">
           <i className="fa fa-power-off"></i>
-          <a href="#">Log out</a>
+          <a href="../">Log out</a>
         </div>
       </div>
     </div>
