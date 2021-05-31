4974d0a72823f4c8bd076cceff3ecd7b771f541d
jonedwards_dot_tech <jon@jonedwards.tech>
2021-05-31 10:29:39 +0000

    Updates carousel with 3D animation movement

--- src/pages/swiper.js
+++ src/pages/swiper.js
@@ -7,39 +7,39 @@ console.clear();
 
 const slides = [
   {
-    title: "Machu Picchu",
-    subtitle: "Peru",
-    description: "Adventure is never far away",
+    title: "My Dashboard",
+    subtitle: "subtitle",
+    description: "description",
     image:
-      "https://images.unsplash.com/photo-1571771019784-3ff35f4f4277?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      "https://jonedwards.tech/img/empyrean/gameStats/gameDashboard1.png"
   },
   {
-    title: "Chamonix",
-    subtitle: "France",
-    description: "Let your dreams come true",
+    title: "My Stats",
+    subtitle: "subtitle",
+    description: "description",
     image:
-      "https://images.unsplash.com/photo-1581836499506-4a660b39478a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      "https://jonedwards.tech/img/empyrean/gameStats/gameDashboard2.png"
   },
   {
-    title: "Mimisa Rocks",
-    subtitle: "Australia",
-    description: "A piece of heaven",
+    title: "Title",
+    subtitle: "subtitle",
+    description: "description",
     image:
-      "https://images.unsplash.com/photo-1566522650166-bd8b3e3a2b4b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      "https://jonedwards.tech/img/empyrean/gameStats/gameDashboard3.png"
   },
   {
-    title: "Four",
-    subtitle: "Australia",
-    description: "A piece of heaven",
+    title: "My Money",
+    subtitle: "subtitle",
+    description: "description",
     image:
-      "https://images.unsplash.com/flagged/photo-1564918031455-72f4e35ba7a6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      "https://jonedwards.tech/img/empyrean/gameStats/gameDashboard4.png"
   },
   {
-    title: "Five",
-    subtitle: "Australia",
-    description: "A piece of heaven",
+    title: "My Games",
+    subtitle: "subtitle",
+    description: "description",
     image:
-      "https://images.unsplash.com/photo-1579130781921-76e18892b57b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+      "https://jonedwards.tech/img/empyrean/gameStats/gameDashboard5.png"
   }
 ];
 
@@ -119,12 +119,12 @@ function Slide({ slide, offset }) {
         "--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1
       }}
     >
-      <div
+      {/* <div
         className="slideBackground"
         style={{
           backgroundImage: `url('${slide.image}')`
         }}
-      />
+      /> */}
       <div
         className="slideContent"
         style={{
