25d735a89c43161a26cb37fbd58652723ab1c776
Jon1969Edwards <jon1969edwards@gmail.com>
2021-08-27 13:48:02 +0200

    Updates

--- src/theme.js
+++
@@ -1,7 +0,0 @@
-export const theme = {
-    primaryDark: '#0D0C1D',
-    primaryLight: '#EFFFFA',
-    primaryHover: '#343078',
-    primaryTransparent: 'rgba(0,0,0,0.5)',
-    mobile: '576px',
-  }
